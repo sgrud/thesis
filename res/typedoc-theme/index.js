a37dfc2886c239a52b05c64d8f48f0c65662b467
Philip Schildkamp <philip.schildkamp@uni-koeln.de>
2023-06-12 00:41:05 +0200

    Implement custom `typedoc-plugin-markdown` template

--- res/typedoc-theme/index.js
+++ res/typedoc-theme/index.js
@@ -1,58 +1,141 @@
-const { helpers } = require('handlebars');
-const { ReflectionKind, UrlMapping } = require('typedoc');
+const { readFileSync } = require('fs');
+const { compile, helpers } = require('handlebars');
+const { Converter, ReflectionKind, UrlMapping } = require('typedoc');
 const { MarkdownTheme } = require('typedoc-plugin-markdown');
 const utils = require('typedoc-plugin-markdown/dist/utils');
 
+/**
+ * @param {import('typedoc').Application} app - A TypeDoc application reference.
+ * @returns {import('typedoc-plugin-markdown').MarkdownTheme} The TypeDoc theme.
+ */
 exports.load = (app) => app.renderer.theme = new class extends MarkdownTheme {
+
+  static {
+    app.converter.on(Converter.EVENT_RESOLVE_END, ({ project }) => {
+      for (const key in project.reflections) {
+        const { comment } = project.reflections[key];
+
+        if (comment?.blockTags.length) {
+          for (let i = 0; i < comment.blockTags.length; i++) {
+            const { content, tag } = comment.blockTags[i];
+
+            if (tag === '@decorator') {
+              const n = comment.blockTags.findIndex((j) => j.tag === tag);
+
+              if (i > n) {
+                content.unshift({ kind: 'text', text: ', ' });
+                comment.blockTags[n].content.push(...content);
+                comment.blockTags.splice(i--, 1);
+              }
+            } else if (tag === '@defaultValue') {
+              if (!project.reflections[key].defaultValue) {
+                for (const { kind, text } of content) {
+                  if (kind === 'code') {
+                    try {
+                      const value = eval(text.replace(/^`(.+)`$/, '$1'));
+                      project.reflections[key].defaultValue = value;
+                      break;
+                    } catch {
+                      continue;
+                    }
+                  }
+                }
+              }
+
+              comment.blockTags.splice(i--, 1);
+            } else if (tag === '@see' && content.length > 1) {
+              for (let j = 0; j < content.length - 1; j++) {
+                switch (content[j].text) {
+                  case ' - ': content.splice(j--, 1); break;
+                  case '\n': content[j].text = ', '; break;
+                }
+              }
+            }
+          }
+        }
+      }
+    });
+  }
 
   constructor(renderer) {
     super(renderer);
 
-    const { escapeChars } = utils; utils.escapeChars = function(...args) {
-      return escapeChars.apply(this, args).replace(/\$/g, '\\$');
-    }
-
-    const { type } = helpers; helpers.type = function(...args) {
-      return type.apply(this, args).replace(/\\>\\>/g, '\\>⁠\\>');
-    }
+    this.template = compile(readFileSync(`${__dirname}/template.hbs`, 'utf8'));
 
     this.entryDocument = 'typedoc.md';
-    this.template = this.getReflectionMemberTemplate();
+    this.hideMembersSymbol = true;
 
-    this.hideBreadcrumbs = true;
-    this.hideInPageTOC = true;
-    this.namedAnchors = true;
-
-    this.spooler = [];
+    this.pages = [];
     this.urls = [];
-  }
 
-  getMeta(reflection) {
-    let { anchor, name, parent } = reflection;
+    const { comments, hierarchy, relativeURL, signatureTitle, type } = helpers;
+    const { escapeChars } = utils;
 
-    while (parent && parent.kind !== ReflectionKind.Project) {
-      name = `${parent.name}.${name}`;
-      parent = parent.parent;
-    }
+    Object.assign(helpers, {
+      comments(comment) {
+        const value = comments.call(this, comment);
 
-    const index = reflection.name.replace(/^_/, '');
-    const label = `\\textbf{\\detokenize{${index}}}`;
-    const level = name.split('.').slice(0, -1).join('.');
-    const refer = `\\detokenize{${level || 'global'}}`;
+        return value.replace(/`\*\*$/gm, '`**\\nopagebreak');
+      },
+      depth() {
+        const value = [];
+        let reflection = this;
 
-    return ` []{#${anchor}}\n\\index{${index}@${label}!${refer}}`;
+        while (reflection = reflection.parent) {
+          value.push('#');
+        }
+
+        return value.join('');
+      },
+      hierarchy(param) {
+        const level = Number.isInteger(param) ? param : 0;
+        const value = hierarchy.call(this, level).replace(/↳/g, '-');
+
+        return value.replace(/`\*\*$/gm, '`**\\nopagebreak');
+      },
+      kind() {
+        const { flags, kind } = this;
+
+        return [...flags, ReflectionKind.singularString(kind)].join(' ');
+      },
+      parents() {
+        const value = [];
+        let reflection = this;
+
+        while ((reflection = reflection.parent) && reflection.parent) {
+          value.unshift(`[${reflection.name}](${relativeURL(reflection.url)})`);
+        }
+
+        return value.join('.') || 'global';
+      },
+      signature(prefix) {
+        const index = this.parent.flags.reduce((i, j) => i + j.length + 2, 0);
+        const value = signatureTitle.call(this, prefix);
+
+        return value.substring(index + this.parent.flags.length);
+      },
+      type(collapse, emphasis) {
+        const value = type.call(this, collapse, emphasis);
+
+        return value.replace(/\\>/g, '&zwnj;>');
+      }
+    });
+
+    Object.assign(utils, {
+      escapeChars(str) {
+        const value = escapeChars.call(this, str);
+
+        return value.replace(/\\`/g, '`');
+      }
+    });
   }
 
   getUrls(reflection, urls = this.urls) {
-    if (reflection.kind !== ReflectionKind.Project) {
+    if (!reflection.kindOf(ReflectionKind.Project)) {
       reflection.anchor = this.getUrl(reflection);
       reflection.url = '#' + reflection.anchor;
 
-      urls.push(new UrlMapping(
-        this.entryDocument,
-        reflection,
-        this.template
-      ));
+      urls.push(new UrlMapping(this.entryDocument, reflection));
     }
 
     if (reflection.children?.length) {
@@ -64,21 +147,15 @@ exports.load = (app) => app.renderer.theme = new class extends MarkdownTheme {
     return urls;
   }
 
-  render(page, template) {
-    this.hidePageTitle = page.model.kind !== ReflectionKind.Module;
-
-    const render = super.render(page, template);
-    const splice = /(?<=^##.*$)/m.exec(render).index;
-
-    this.spooler.push([
-      render.substring(0, splice),
-      this.getMeta(page.model),
-      render.substring(splice),
-    ].join(''));
+  render(page) {
+    this.pages.push(super.render(page, (data) => this.template(data, {
+      allowProtoMethodsByDefault: true,
+      allowProtoPropertiesByDefault: true
+    })));
 
     return page.model === this.urls[this.urls.length - 1]?.model
-      ? this.spooler.join('\n')
-      : new Buffer(0);
+      ? this.pages.join('\n')
+      : '';
   }
 
 }(app.renderer);
